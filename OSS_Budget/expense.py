27919a4f8547d36d6749809e60d2588837e737f9
KSB\justinksb <justinksb@naver.com>
2025-11-30 01:29:08 +0900

    expense.py: Expense 클래스 생성자(__init__) 및 __str__ 메서드에 'payment_method' 필드를 추가하여 데이터 구조를 확장함.

--- OSS_Budget/expense.py
+++ OSS_Budget/expense.py
@@ -1,10 +1,11 @@
 
 class Expense:
-    def __init__(self, date, category, description, amount):
+    def __init__(self, date, category, description, amount, payment_method):
         self.date = date
         self.category = category
         self.description = description
         self.amount = amount
+        self.payment_method = payment_method
 
     def __str__(self):
-        return f"[{self.date}] {self.category} - {self.description}: {self.amount}원"
+        return f"[{self.date}] ({self.category}) [{self.payment_method}] {self.description}: {self.amount}₩"
